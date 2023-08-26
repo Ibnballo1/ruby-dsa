6f7df6cfd7fe3f834cb6fe3f469eb12aab79950c
Ibnballo1 <belloabdullateef035@gmail.com>
2023-08-26 09:14:48 +0100

    Solve stringy challenge

--- stringy-strings/stringy_strings.rb
+++ stringy-strings/stringy_strings.rb
@@ -1,15 +1,23 @@
+# def stringy(size)
+#   # Your code here
+#   new_str = ''
+#   size.times do |x|
+#     if x%2 == 0
+#       new_str += '1'
+#     else
+#       new_str += '0'
+#     end
+#   end
+#   new_str
+# end
+
+# p stringy(6)
+
 def stringy(size)
-  # Your code here
   new_str = ''
-  size.times do |x|
-    if x%2 == 0
-      new_str += '1'
-    else
-      new_str += '0'
-    end
-  end
+  size.times {|x| (x % 2 == 0) ? new_str += '1' : new_str += '0' }
   new_str
 end
-
-p stringy(6)
+  
+puts stringy(6)
   
