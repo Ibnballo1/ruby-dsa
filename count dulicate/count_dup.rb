21b835f762a294c86018905c9f9eb1844b1dc1c0
Ibnballo1 <belloabdullateef035@gmail.com>
2023-09-09 07:51:06 +0100

    Update

--- count dulicate/count_dup.rb
+++ count dulicate/count_dup.rb
@@ -1,11 +1,8 @@
 def duplicate_count(text)
-  #your code here
   counter = 0
   previous_char = []
   text.downcase.chars do |chr|
-    # puts "chr is #{chr} and prev char is #{previous_char} and counter is #{counter}"
     next if previous_char.include?(chr)
-    # puts "After break, chr is #{chr} and counter is #{counter}"
     breaker = 0
     text.downcase.each_char do |chr2|
       if chr == chr2
@@ -20,10 +17,6 @@ def duplicate_count(text)
   end
   counter
 end
-
-# def duplicate_count(text)
-#   text.downcase.chars.group_by(&:itself).count { |_, group| group.length > 1 }
-# end
 
 p duplicate_count("abcde") # => 0
 p duplicate_count("aabbcde") # => 2
