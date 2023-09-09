21dabd879598e69bb1ca9f0738f8d97a0819d50d
Ibnballo1 <belloabdullateef035@gmail.com>
2023-09-09 07:50:18 +0100

    Solve count_duplicae

--- count dulicate/count_dup.rb
+++ count dulicate/count_dup.rb
@@ -2,8 +2,10 @@ def duplicate_count(text)
   #your code here
   counter = 0
   previous_char = []
-  text.downcase.each_char do |chr|
-    break if previous_char.include?(chr)
+  text.downcase.chars do |chr|
+    # puts "chr is #{chr} and prev char is #{previous_char} and counter is #{counter}"
+    next if previous_char.include?(chr)
+    # puts "After break, chr is #{chr} and counter is #{counter}"
     breaker = 0
     text.downcase.each_char do |chr2|
       if chr == chr2
@@ -26,7 +28,7 @@ end
 p duplicate_count("abcde") # => 0
 p duplicate_count("aabbcde") # => 2
 p duplicate_count("aabBcde") # => 2
-# p duplicate_count("indivisibility") # => 1
-# p duplicate_count("Indivisibilities") # => 2
-# p duplicate_count("aA11") # => 2
+p duplicate_count("indivisibility") # => 1
+p duplicate_count("Indivisibilities") # => 2
+p duplicate_count("aA11") # => 2
 p duplicate_count("ABBA") # => 2
