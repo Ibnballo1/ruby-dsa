e43765f19706d928d23ef824494a39a6f7324dfe
Ibnballo1 <belloabdullateef035@gmail.com>
2023-09-22 19:58:42 +0100

    Solve human readable duration format

--- human-readable-duration-format/format_duration.rb
+++ human-readable-duration-format/format_duration.rb
@@ -9,60 +9,39 @@ def format_duration(seconds)
   while seconds > 59
     if  seconds >= yr_in_secs
       year = seconds / yr_in_secs
-      result += "#{year} year#{pluralize(year)}"
+      result += "#{year} year#{pluralize(year)}, "
       seconds %= yr_in_secs
-      if (seconds / days_in_secs > 0 || seconds / hr_in_secs > 0 || seconds / min_in_sec > 0) && seconds > 0
-          result += ', '
-      elsif seconds == 0
-          result
-      else
-          result += ' and '
-      end
     elsif seconds >= days_in_secs
       days = seconds / days_in_secs
-      result += "#{days} day#{pluralize(days)}"
+      result += "#{days} day#{pluralize(days)}, "
       seconds %= days_in_secs
-      if (seconds / hr_in_secs > 0 || seconds / min_in_sec > 0) && seconds > 0
-          result += ', '
-      elsif seconds == 0
-          result
-      else
-          result += ' and '
-      end
     elsif seconds >= hr_in_secs
       hours = seconds / hr_in_secs
-      result += "#{hours} hour#{pluralize(hours)}"
+      result += "#{hours} hour#{pluralize(hours)}, "
       seconds %= hr_in_secs
-      puts "seconds after modulus is #{seconds} and seconds / min_in_sec is #{seconds / min_in_sec}"
-      if seconds / min_in_sec > 0
-          result += ' and '
-      elsif seconds == 0
-          result
-      else
-          result += ', '
-      end
     elsif seconds >= min_in_sec
       mins = seconds / min_in_sec
-      result += "#{mins} minute#{pluralize(mins)}"
+      result += "#{mins} minute#{pluralize(mins)}, "
       seconds %= min_in_sec
-      result += ' and ' if seconds > 0
     end
   end
   if seconds > 0
     result += "#{seconds} second#{pluralize(seconds)}"
   else
-    result
+    result = result.chomp(', ')
   end
+
+  result.sub(/,([^,]+)$/, ' and\1') # Replace the last comma with "and"
 end
 
-# p format_duration(31536700 * 2) # => "2 years, 23 minutes and 20 seconds"
-# p format_duration(72) # => "1 minute and 12 seconds"
-# p format_duration(2890) # => "48 minutes and 10 seconds"
-# p format_duration(60) # => "1 minute"
-# p format_duration(62) # => "1 minute and 2 seconds"
-# p format_duration(0) # => "now"
-# p format_duration(2) # => 2 seconds
+p format_duration(31536700 * 2) # => "2 years, 23 minutes and 20 seconds"
+p format_duration(72) # => "1 minute and 12 seconds"
+p format_duration(2890) # => "48 minutes and 10 seconds"
+p format_duration(60) # => "1 minute"
+p format_duration(62) # => "1 minute and 2 seconds"
+p format_duration(0) # => "now"
+p format_duration(2) # => 2 seconds
 p format_duration(3662) # => "1 hour, 1 minute and 2 seconds"
-# p format_duration(3600) # => "1 hour"
+p format_duration(3600) # => "1 hour"
 p format_duration(132030240) # => "4 years, 68 days, 3 hours and 4 minutes"
 p format_duration(583211) # => "6 days, 18 hours and 11 seconds"
